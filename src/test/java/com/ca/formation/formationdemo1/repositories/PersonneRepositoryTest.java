0bcb3bca2bee15053a6b6866f1aa30ea5f1e9661
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2022-12-28 18:36:24 +0100

    Update PersonneRepositoryTest.java

--- src/test/java/com/ca/formation/formationdemo1/repositories/PersonneRepositoryTest.java
+++ src/test/java/com/ca/formation/formationdemo1/repositories/PersonneRepositoryTest.java
@@ -33,7 +33,7 @@ public class PersonneRepositoryTest {
     //Given
     Personne pers = personneRepository.save(new Personne("malcom", "mbaye", 12));
     //When
-    Optional<Personne> person = personneRepository.findById(pers.getId());
+      Personne person = personneRepository.findById(pers.getId());
     //Then
     assertNotNull(person);
     assertEquals("malcom", person.get().getNom());
