ccba0ada6462c02491ad6e6d60dfd020ec7daead
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2022-12-30 13:04:17 +0100

    Update PersonneController.java

--- src/main/java/com/ca/formation/formationdemo1/controllers/PersonneController.java
+++ src/main/java/com/ca/formation/formationdemo1/controllers/PersonneController.java
@@ -1,10 +1,11 @@
 package com.ca.formation.formationdemo1.controllers;
 
 import com.ca.formation.formationdemo1.models.Personne;
+import com.ca.formation.formationdemo1.models.PersonneRequest;
+
 import com.ca.formation.formationdemo1.repositories.PersonneRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import com.ca.formation.formationdemo1.models.PersonneRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,17 +25,17 @@ public class PersonneController {
     }
 
     @GetMapping("/nouveau")
-    public String nouveauPersonne(){
+    public String nouveauPersonne(PersonneRequest personne){
         return "nouveau";
     }
 
     @PostMapping("/ajouterPersonne")
-    public String ajouterPersonne(PersonneRequest personne){
+    public String ajouterPersonne(PersonneRequest personne, Model model){
         Personne pers = new Personne();
         pers.setNom(personne.getNom());
         pers.setPrenom(personne.getPrenom());
         pers.setAge(personne.getAge());
-        repository.save(personne);
+        repository.save(pers);
         return "redirect:/";
 
     }
