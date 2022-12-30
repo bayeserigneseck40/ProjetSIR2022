850ae51b1bc46eb01adfdd400cf0c7ce02fc5c3b
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2022-12-30 17:52:16 +0100

    Update PersonneControllerTest.java

--- src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
+++ src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
@@ -60,20 +60,6 @@ public class PersonneControllerTest {
 
     }
 
-    @Test
-    @WithMockUser(username = "michel@formation.sn", password = "Passer@123", authorities = { "READ" })
-    public void helloTest() {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + tokenRequest);
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-        ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/api/v2/personnes/bye",
-                HttpMethod.GET, entity, String.class);
-
-        assertNotEquals(response.getBody(),"Bye bye");
-    }
-
 
     @Test
     @WithMockUser(username = "michel@formation.sn", password = "Passer@123", authorities = { "READ" })
