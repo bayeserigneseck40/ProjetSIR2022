cce5f110909819f9355fc1937d9ffa151cb24d2b
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2022-12-30 19:05:43 +0100

    Update PersonneControllerTest.java

--- src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
+++ src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
@@ -3,6 +3,7 @@ package com.ca.formation.formationdemo1.controllers;
 import com.ca.formation.formationdemo1.models.Personne;
 import com.ca.formation.formationdemo1.services.PersonneService;
 import com.ca.formation.formationdemo1.models.Utilisateur;
+import com.ca.formation.formationdemo1.models.Role;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
