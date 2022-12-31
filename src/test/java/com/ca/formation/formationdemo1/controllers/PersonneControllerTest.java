9d991ae873a96d0ae7cf7edad1920cb847804cba
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2022-12-31 22:58:55 +0100

    Update PersonneControllerTest.java

--- src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
+++ src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
@@ -86,6 +86,17 @@ public class PersonneControllerTest {
         mockMvc.perform(requestBuilder).andExpect(status().isUnauthorized());
 
     }
+    @Test
+    @WithMockUser(username = "clara@formation.ca", password = "Passer@123", authorities = {"READ"})
+    public void nouveauPersonne() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/nouveau")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+          String response = mvcResult.getResponse().getContentAsString();
+        assertEquals("nouveau",response);
+    }
 
     @Test
     @WithMockUser(username = "michel@formation.sn", password = "Passer@123", authorities = { "READ" })
