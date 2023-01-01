b7c880ab3707a006a4c34396d1517a13c3057214
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2023-01-01 13:10:32 +0100

    Update PersonneController.java

--- src/main/java/com/ca/formation/formationdemo1/controllers/PersonneController.java
+++ src/main/java/com/ca/formation/formationdemo1/controllers/PersonneController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+@RestController
 public class PersonneController {
 
     private final PersonneRepository repository;
