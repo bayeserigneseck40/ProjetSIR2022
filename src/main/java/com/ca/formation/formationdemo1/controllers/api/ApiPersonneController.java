c32d2652adc67578f67a830170b00a5ff36a553f
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2023-01-01 14:22:27 +0100

    Update ApiPersonneController.java

--- src/main/java/com/ca/formation/formationdemo1/controllers/api/ApiPersonneController.java
+++ src/main/java/com/ca/formation/formationdemo1/controllers/api/ApiPersonneController.java
@@ -2,7 +2,6 @@ package com.ca.formation.formationdemo1.controllers.api;
 
 import com.ca.formation.formationdemo1.exception.ResourceNotFoundException;
 import com.ca.formation.formationdemo1.models.Personne;
-import com.ca.formation.formationdemo1.models.PersonneDTO;
 import com.ca.formation.formationdemo1.models.Role;
 import com.ca.formation.formationdemo1.services.PersonneService;
 import org.springframework.http.ResponseEntity;
@@ -61,16 +60,15 @@ public class ApiPersonneController {
     }
 
     @PostMapping
-    public ResponseEntity<Personne> addPersonne(@RequestBody PersonneDTO personne){
+    public ResponseEntity<Personne> addPersonne(@RequestBody Personne personne){
         Personne personneResponse = personneService.addPersonne(personne);
         return ResponseEntity.ok().body(personneResponse);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Personne> updatePersonne(@PathVariable(value="id") Long id, @RequestBody PersonneDTO personneRequest ) throws Exception {
-        Personne personne = personneService.updatePersonne(id,personneRequest);
+    public ResponseEntity<Personne> updatePersonne(@PathVariable(value="id") Long id, @RequestBody Personne personneRequest ) throws Exception {
 
-        return ResponseEntity.ok().body(personne);
+        return ResponseEntity.ok().body(personneRequest);
     }
 
     @DeleteMapping("/{id}")
