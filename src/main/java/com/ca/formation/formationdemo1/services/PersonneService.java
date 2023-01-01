4b4f723d507e42dfb492e2f3dc837feda849eb4f
bayembacke221 <mbackembaye74@gmail.com>
2023-01-01 12:46:06 +0000

    add config

--- src/main/java/com/ca/formation/formationdemo1/services/PersonneService.java
+++ src/main/java/com/ca/formation/formationdemo1/services/PersonneService.java
@@ -2,6 +2,7 @@ package com.ca.formation.formationdemo1.services;
 
 import com.ca.formation.formationdemo1.exception.ResourceNotFoundException;
 import com.ca.formation.formationdemo1.models.Personne;
+import com.ca.formation.formationdemo1.models.PersonneDTO;
 
 import java.util.List;
 
@@ -11,9 +12,9 @@ public interface PersonneService {
 
     Personne getPersonne(Long id) throws ResourceNotFoundException;
 
-    Personne updatePersonne(Long id, Personne personne) throws ResourceNotFoundException ;
+    Personne updatePersonne(Long id, PersonneDTO personne) throws ResourceNotFoundException ;
 
-    Personne addPersonne(Personne personne);
+    Personne addPersonne(PersonneDTO personne);
 
     void deletePersonne(Long id);
 
