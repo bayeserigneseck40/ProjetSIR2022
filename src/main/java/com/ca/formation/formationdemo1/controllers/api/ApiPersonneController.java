8a2837ec9df04a0521e3f484026f6ab7c9c397d2
Baye serigne seck <96342979+bayeserigneseck40@users.noreply.github.com>
2023-01-03 18:53:01 +0100

    Update ApiPersonneController.java

--- src/main/java/com/ca/formation/formationdemo1/controllers/api/ApiPersonneController.java
+++ src/main/java/com/ca/formation/formationdemo1/controllers/api/ApiPersonneController.java
@@ -94,5 +94,20 @@ public class ApiPersonneController {
     }
 
 
+    @GetMapping("/ccc")
+    public List<Personne> getPersonneParNomAndPrenom(@RequestParam(name = "nom")String nom,@RequestParam(name = "prenom")String prenom) {
+        return  personneService.getPersonneParNomAndPrenom(nom,prenom);
+    }
+
+    @GetMapping("/aaa")
+    public List<Personne> getPersonneNomAndPrenom(@RequestParam(name = "nom")String nom, @RequestParam(name = "prenom")String prenom) {
+        return  personneService.getPersonneNomAndPrenom(nom,prenom);
+    }
+
+    @GetMapping("/bbb")
+    public List<Personne> getPersonneNomAndPrenom2(@RequestParam(name = "nom")String nom,@RequestParam(name = "prenom") String prenom) {
+        return  personneService.getPersonneNomAndPrenom2(nom,prenom);
+    }
+
 
 }
